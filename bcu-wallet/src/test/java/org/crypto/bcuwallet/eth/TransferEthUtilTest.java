5d5dd0239aadee9adb376164c316914e807c8210
dkz97 <r2cryptocn@gmail.com>
2022-08-01 15:27:09 +0800

    delete transfer test method

--- bcu-wallet/src/test/java/org/crypto/bcuwallet/eth/TransferEthUtilTest.java
+++ bcu-wallet/src/test/java/org/crypto/bcuwallet/eth/TransferEthUtilTest.java
@@ -51,69 +51,6 @@ class TransferEthUtilTest {
         assertFalse(TransferEthUtil.transfer(web3j, credentials, "42 Main St", BigDecimal.valueOf(1L)));
     }
 
-    /**
-     * Method under test: {@link TransferEthUtil#transfer(Web3j, Credentials, String, BigDecimal)}
-     */
-    @Test
-    void testTransfer2() throws Exception {
-        assertFalse(TransferEthUtil.transfer(null, null, "foo", BigDecimal.valueOf(1L)));
-    }
-
-    /**
-     * Method under test: {@link TransferEthUtil#transfer(Web3j, Credentials, String, BigDecimal)}
-     */
-    @Test
-    void testTransfer3() throws Exception {
-        JsonRpc2_0Admin web3j = new JsonRpc2_0Admin(null);
-        Credentials credentials = mock(Credentials.class);
-        assertFalse(TransferEthUtil.transfer(web3j, credentials, "42 Main St", BigDecimal.valueOf(1L)));
-    }
-
-    /**
-     * Method under test: {@link TransferEthUtil#transfer(Web3j, Credentials, String, BigDecimal)}
-     */
-    @Test
-    void testTransfer4() throws Exception {
-        JsonRpc2_0Admin web3j = new JsonRpc2_0Admin(new HttpService());
-        assertFalse(TransferEthUtil.transfer(web3j, null, "42 Main St", BigDecimal.valueOf(1L)));
-    }
-
-    /**
-     * Method under test: {@link TransferEthUtil#transfer(Web3j, Credentials, String, BigDecimal)}
-     */
-    @Test
-    @Disabled("TODO: Complete this test")
-    void testTransfer5() throws Exception {
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException
-        //       at java.math.BigInteger.<init>(BigInteger.java:415)
-        //       at org.web3j.utils.Numeric.toBigIntNoPrefix(Numeric.java:123)
-        //       at org.web3j.crypto.WalletUtils.isValidAddress(WalletUtils.java:233)
-        //       at org.web3j.crypto.WalletUtils.isValidAddress(WalletUtils.java:226)
-        //       at org.crypto.bcuwallet.eth.TransferEthUtil.transfer(TransferEthUtil.java:55)
-        //   In order to prevent transfer(Web3j, Credentials, String, BigDecimal)
-        //   from throwing NullPointerException, add constructors or factory
-        //   methods that make it easier to construct fully initialized objects used in
-        //   transfer(Web3j, Credentials, String, BigDecimal).
-        //   See https://diff.blue/R013 to resolve this issue.
-
-        JsonRpc2_0Admin web3j = new JsonRpc2_0Admin(new HttpService());
-        Credentials credentials = mock(Credentials.class);
-        TransferEthUtil.transfer(web3j, credentials, null, BigDecimal.valueOf(1L));
-    }
-
-    /**
-     * Method under test: {@link TransferEthUtil#transfer(Web3j, Credentials, String, BigDecimal)}
-     */
-    @Test
-    void testTransfer6() throws Exception {
-        JsonRpc2_0Admin web3j = new JsonRpc2_0Admin(new HttpService());
-        Credentials credentials = mock(Credentials.class);
-        assertFalse(TransferEthUtil.transfer(web3j, credentials, "42", BigDecimal.valueOf(1L)));
-    }
 
 
 }
