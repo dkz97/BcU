980ee1cb6c4726e2ab9cb4aa4ac266e6720fb81a
dkz97 <r2cryptocn@gmail.com>
2022-08-16 09:41:53 +0800

    add testGetBalanceOf test method

--- bcu-wallet/src/test/java/org/crypto/bcuwallet/account/AccountUtilTest.java
+++ bcu-wallet/src/test/java/org/crypto/bcuwallet/account/AccountUtilTest.java
@@ -1,31 +1,21 @@
 package org.crypto.bcuwallet.account;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 import org.crypto.bcuwallet.account.struct.AccountStruct;
-import org.junit.jupiter.api.Disabled;
+import org.crypto.bcuwallet.common.util.Web3jUtil;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.Web3jService;
 import org.web3j.protocol.admin.JsonRpc2_0Admin;
-import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.Request;
-import org.web3j.protocol.core.Response;
-import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.http.HttpService;
-import org.web3j.protocol.websocket.WebSocketService;
 
 class AccountUtilTest {
     /**
@@ -41,11 +31,18 @@ class AccountUtilTest {
         assertEquals(5, account.size());
     }
 
+    @Test
+    void testGetBalanceOf() throws IOException {
+        Web3j web3j = Web3jUtil.connectEthLocal();
+        String account = "321";
+        AccountUtil.getBalanceOf(web3j, account);
+    }
+
     /**
      * Method under test: {@link AccountUtil#getBalanceOf(Web3j, List)}
      */
     @Test
-    void testGetBalanceOf() throws IOException {
+    void testGetBalanceOf1() throws IOException {
         JsonRpc2_0Admin web3j = new JsonRpc2_0Admin(new HttpService());
         assertTrue(AccountUtil.getBalanceOf(web3j, new ArrayList<>()).isEmpty());
     }
